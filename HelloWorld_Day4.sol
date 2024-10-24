ce9bd02562fae2302f32ca37d5296d1a03bd74b7
Masha <myakoven@student.42berlin.de>
2024-10-24 16:14:20 +0200

    udating helloword

--- HelloWorld_Day4.sol
+++ HelloWorld_Day4.sol
@@ -3,48 +3,90 @@ pragma solidity >=0.7.0 <0.9.0;
 
 interface IHelloWorld {
 
+    function helloWorld() external view returns (string memory);
 
-    function helloWorld() external view returns (string memory) ;
+    function setText(string calldata newText) external;
 
-    function setText(string calldata newText) external ;
+    function initialText() external pure returns (string memory);
 
-    function transferOwnership(address newOwner) public onlyOwner ;
+    function potato() external;
 
-    function potato;
+    function rainbow() external;
+
 }
 
 contract HelloWorld {
     string private text;
-    address public owner;
-
-    modifier onlyOwner()
-    {
-        require (msg.sender == owner, "Caller is not the owner");
-        _;
-    }
 
     constructor() {
-        text = "Hello World";
-        owner = msg.sender;
+        text = initialText();
     }
 
-    function helloWorld() public view returns (string memory) {
+    function helloWorld() external view returns (string memory) {
         return text;
     }
 
-    //
-    function setText(string calldata newText) public onlyOwner {
-        // if (block.number % 2 == 0){ 
-        //     newText = "Even";
-        // }
-        ///not possible with calldata - it needs to be a mutex in memory
-        // call data does not use memory
+    function setText(string calldata newText) public {
         text = newText;
     }
 
-    function transferOwnership(address newOwner) public onlyOwner {
-        owner = newOwner;
+    function initialText() public pure virtual returns (string memory) {
+        return "Hello World";
     }
 
+    fallback() external {
+        text = "Oops, wrong function name called";
+    }
+
+    receive() external payable {
+        text = "I'm Rich!";
+    }
 
 }
+
+contract Greeter {
+
+    function invokeGreeting(address target) external view returns (tring memory){
+        return IHelloWorld(target).helloWorld();
+    }
+
+    function setGreeter(address target, string calldata newText) public {
+        IHelloWorld(target).setext(newText);
+    }
+}
+
+
+// contract HelloWorld {
+//     string private text;
+//     address public owner;
+
+//     modifier onlyOwner()
+//     {
+//         require (msg.sender == owner, "Caller is not the owner");
+//         _;
+//     }
+
+//     constructor() {
+//         text = "Hello World";
+//         owner = msg.sender;
+//     }
+
+//     function helloWorld() public view returns (string memory) {
+//         return text;
+//     }
+
+//     //
+//     function setText(string calldata newText) public onlyOwner {
+//         // if (block.number % 2 == 0){
+//         //     newText = "Even";
+//         // }
+//         ///not possible with calldata - it needs to be a mutex in memory
+//         // call data does not use memory
+//         text = newText;
+//     }
+
+//     function transferOwnership(address newOwner) public onlyOwner {
+//         owner = newOwner;
+//     }
+
+// }
